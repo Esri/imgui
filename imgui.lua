392eaf13d31f20431c428d566618f226817b6ef3
Christian O. Venegas <cvenegas@esri.com>
2021-03-11 16:19:54 -0800

    arch: Make changes needed for the new premake build structure
    The premake build structure has been simplified and rewritten to reduce
    the boilerplate needed to add additional configurations while forcing
    the unique settings of a project to be defined. Migrate some defines
    and compiler options to the global settings and remove all the old
    boilerplate from this project.

--- imgui.lua
+++ imgui.lua
@@ -1,206 +1,53 @@
 project "imgui"
 
-  local prj = project()
-  local prjDir = prj.basedir
+dofile(_BUILD_DIR .. "/static_library.lua")
 
-  -- -------------------------------------------------------------
-  -- project
-  -- -------------------------------------------------------------
+configuration { "*" }
 
-  -- common project settings
+uuid "00273C6F-C6D6-456A-9DFC-B65011816616"
 
-  dofile (_BUILD_DIR .. "/3rdparty_static_project.lua")
+includedirs {
+  ".",
+  _3RDPARTY_DIR,
+}
 
-  -- project specific settings
+files {
+  "*.cpp",
+  "examples/imgui_impl_glfw.cpp",
+  "examples/imgui_impl_opengl3.cpp",
+  "examples/imgui_impl_opengl2.cpp",
+}
 
-  uuid "00273C6F-C6D6-456A-9DFC-B65011816616"
+if (_PLATFORM_ANDROID) then
+end
+
+if (_PLATFORM_COCOA) then
+end
+
+if (_PLATFORM_IOS) then
+end
+
+if (_PLATFORM_LINUX) then
+end
+
+if (_PLATFORM_MACOS) then
+  buildoptions {
+    "-fobjc-arc",
+    "-fobjc-arc-exceptions",
+  }
 
   files {
-    "*.cpp",
-    "examples/imgui_impl_glfw.cpp",
-    "examples/imgui_impl_opengl3.cpp",
-    "examples/imgui_impl_opengl2.cpp",
+    "examples/imgui_impl_metal.mm",
+    "examples/imgui_impl_osx.mm",
   }
+end
 
-  includedirs {
-    prjDir,
-    _3RDPARTY_DIR,
+if (_PLATFORM_WINDOWS) then
+  files {
+    "examples/imgui_impl_dx11.cpp",
+    "examples/imgui_impl_win32.cpp",
   }
+end
 
-  -- -------------------------------------------------------------
-  -- configurations
-  -- -------------------------------------------------------------
-
-  if (_PLATFORM_WINDOWS) then
-    -- -------------------------------------------------------------
-    -- configuration { "windows" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/3rdparty_static_win.lua")
-
-    -- project specific configuration settings
-
-    configuration { "windows" }
-
-      files {
-        "examples/imgui_impl_dx11.cpp",
-        "examples/imgui_impl_win32.cpp",
-      }
-
-    -- -------------------------------------------------------------
-    -- configuration { "windows", "Debug", "x32" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_win_x86_debug.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "windows", "Debug", "x32" }
-
-    -- -------------------------------------------------------------
-    -- configuration { "windows", "Debug", "x64" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_win_x64_debug.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "windows", "Debug", "x64" }
-
-    -- -------------------------------------------------------------
-    -- configuration { "windows", "Release", "x32" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_win_x86_release.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "windows", "Release", "x32" }
-
-    -- -------------------------------------------------------------
-    -- configuration { "windows", "Release", "x64" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_win_x64_release.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "windows", "Release", "x64" }
-
-    -- -------------------------------------------------------------
-  end
-
-  if (_PLATFORM_LINUX) then
-    -- -------------------------------------------------------------
-    -- configuration { "linux" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_linux.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "linux" }
-
-    -- -------------------------------------------------------------
-    -- configuration { "linux", "Debug", "x64" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_linux_x64_debug.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "linux", "Debug", "x64" }
-
-    -- -------------------------------------------------------------
-    -- configuration { "linux", "Release", "x64" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_linux_x64_release.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "linux", "Release", "x64" }
-
-    -- -------------------------------------------------------------
-  end
-
-  if (_PLATFORM_MACOS) then
-    -- -------------------------------------------------------------
-    -- configuration { "macosx" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_mac.lua")
-
-    -- project specific configuration settings
-
-    configuration { "macosx" }
-
-      buildoptions {
-        "-fobjc-arc",
-        "-fobjc-arc-exceptions",
-      }
-
-      files {
-        "examples/imgui_impl_metal.mm",
-        "examples/imgui_impl_osx.mm",
-      }
-
-    -- -------------------------------------------------------------
-    -- configuration { "macosx", "Debug", "x64" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_mac_x64_debug.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "macosx", "Debug", "x64" }
-
-    -- -------------------------------------------------------------
-    -- configuration { "macosx", "Release", "x64" }
-    -- -------------------------------------------------------------
-
-    -- common configuration settings
-
-    dofile (_BUILD_DIR .. "/static_mac_x64_release.lua")
-
-    -- project specific configuration settings
-
-    -- configuration { "macosx", "Release", "x64" }
-
-    -- -------------------------------------------------------------
-  end
-
-  if (_IS_QT) then
-    -- -------------------------------------------------------------
-    -- configuration { "qt" }
-    -- -------------------------------------------------------------
-
-    local qt_include_dirs = PROJECT_INCLUDE_DIRS
-
-    -- Add additional QT include dirs
-    -- table.insert(qt_include_dirs, <INCLUDE_PATH>)
-
-    createqtfiles(project(), qt_include_dirs)
-
-    -- -------------------------------------------------------------
-  end
+if (_PLATFORM_WINUWP) then
+end
