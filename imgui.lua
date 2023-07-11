8521cf8ff88933330bb2a777c821dc203aa0b234
Gary Thomson <gthomson@esri.com>
2023-07-11 10:19:24 +0100

    Conditional build

--- imgui.lua
+++ imgui.lua
@@ -1,2 +1,7 @@
+-- core functionality
 dofile("imgui_core.lua")
-dofile("imgui_ex.lua")
+
+-- desktop platform integration
+if (_PLATFORM_LINUX or _PLATFORM_MACOS or _PLATFORM_WINDOWS) then
+  dofile("imgui_ex.lua")
+end
